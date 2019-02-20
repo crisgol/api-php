c3d90462f72f06ca2e169a4b5a2724df74252406
Paul Miller <paul@battlestardigital.com>
2019-02-19 19:34:38 -0500

    Added index.php as a placeholder

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP API</title>
+</head>
+<body style='padding: 2em;'>
+	<div>
+		<h1>Welcome!</h1>
+		<p>This is a sample API in PHP, based on the work of Mike Dalisay at Code of Ninja. 99.999% of the code is his.</p>
+		<p>This page is a placeholder. API documentation coming soon.</p>
+	</div>
+	<div style='margin-top: 2em'>
+		--<br />
+		Paul Miller<br />
+		February 19, 2019
+	</div>
+</body>
+</html>
